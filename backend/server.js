8d799a6631ac4096cf75137186118127721e33b6
Shreya Thakar <shreyathakar008@gmail.com>
2023-03-21 18:09:33 +0530

    Cors Added

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,26 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors)
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-  res.setHeader("Access-Control-Allow-Credentials", true)
-  next()
-})
+// app.use("/", (req, res, next) => {
+//   console.log("api running")
+//   next()
+// })
+
+cors.CorsOptions = {
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+  ],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
+  preflightContinue: false,
+}
+
+app.use(cors())
 
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
