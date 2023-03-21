8fbb8ab705f75128b9c24f751782aeeb963a6e5c
Shreya Thakar <shreyathakar008@gmail.com>
2023-03-21 08:09:02 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,12 @@ app.use(express.json())
 
 dotenv.config()
 
-app.use((req, res, next) => {
+app.use(cors)
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+  res.setHeader("Access-Control-Allow-Credentials", true)
   next()
 })
 
@@ -50,9 +55,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(notFound, errorHandler)
-
-if (process.env.NODE_ENV === "production") {
-}
 
 const PORT = process.env.PORT || 5000
 
